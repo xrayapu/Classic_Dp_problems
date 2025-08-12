99020297087d70015420265e14eaf1577454c6da
apu <40669057+xrayapu@users.noreply.github.com>
2025-08-12 23:53:31 +0600

    Update problem_16.py

--- problem_16.py
+++ problem_16.py
@@ -4,22 +4,39 @@
 # output= abce (output is 4 in length ! )
 
 
-def sol(arr, tar):
+def sol(arr, arr2):
     n=len(arr)
-    m=len(tar)
+    m=len(arr2)
     narr=[]
     t=[[0]*(m+1) for _ in range(n+1)]
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if arr[i-1]== tar[j-1]:
+            if arr[i-1]== arr2[j-1]:
                 t[i][j]= 1+t[i-1][j-1]
-                narr.append(arr[i-1])
+                
 
             else:
                 t[i][j]= max(t[i][j-1],t[i-1][j])
+
+
+    i=n
+    j=m
+
+    while i>0 and j>0 :
+        if arr[i-1]== arr2[j-1]: # because array starts from 0
+            narr.append(arr[i-1])
+
+            i-=1
+            j-=1
+
+        elif t[i][j-1] > t[i-1][j]: # go to the maximum side ! 
+            j-=1
+
+        else: i-=1
+
                
 
-    return ''.join(narr) 
+    return ''.join(narr[::-1]) 
 
 print(sol('abcde','abxce')) #output= abce
 
